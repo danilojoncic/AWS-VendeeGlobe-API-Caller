377880d75064243ae34fd9889b60881e96191447
Danilo Joncic <91700718+danilojoncic@users.noreply.github.com>
2025-01-11 16:09:48 +0100

    small fix as the ["time"] was not needed and made errors

--- script.py
+++ script.py
@@ -9,7 +9,6 @@ API_KEY = os.environ['api_key']  # API key stored in the environment variable
 SITE = os.environ['site']  # API endpoint stored in the environment variable
 BUCKET_NAME = os.environ['bucket_name']  # S3 bucket name stored in the environment variable
 
-# Initialize S3 client
 s3_client = boto3.client('s3')
 
 def fetch_data():
@@ -30,6 +29,7 @@ def save_to_s3(data):
     Saves the API response as a JSON file in the specified S3 bucket.
     """
     # Generate filename based on the current timestamp
+    # needs to be fixed later due to the different time zones used
     filename = f"api_response_{datetime.now().strftime('%Y%m%d_%H%M%S')}.json"
     
     # Upload the file to S3
@@ -42,16 +42,13 @@ def save_to_s3(data):
     print(f"Response saved to S3 bucket '{BUCKET_NAME}' as '{filename}'")
 
 def lambda_handler(event, context):
+    # the idea is to have this run 6 times per day 
+    # 07:00 11:00 15:00 19:00 23:00 03:00 (the times the sailboats give us an update on their position)
     try:
-        # Fetch data from the API
-        data = fetch_data()
-        
-        # Save the response to S3
+        data = fetch_data()        
         save_to_s3(data)
         
     except Exception as e:
         raise
-    else:
-        return event['time']
     finally:
         print(f"Check complete at {str(datetime.now())}")
